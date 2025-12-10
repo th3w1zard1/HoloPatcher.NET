b101280b2406e82c2ef8d0dd3560831f1d7bde9f
Benjamin Auquite <halomastar@gmail.com>
2025-12-10 07:24:17 -0600

    feat: align game enum with pykotor variants

--- src/CSharpKOTOR/Common/Game.cs
+++ src/CSharpKOTOR/Common/Game.cs
@@ -1,38 +1,63 @@
 namespace CSharpKOTOR.Common
 {
-
+    // Matching PyKotor implementation at Libraries/PyKotor/src/pykotor/common/misc.py:250-285
+    // Original: class Game(IntEnum):
     /// <summary>
-    /// Represents which KOTOR game the installation is for.
+    /// Represents which KOTOR game / platform variant.
     /// </summary>
     public enum Game
     {
-        /// <summary>
-        /// Knights of the Old Republic (KOTOR 1)
-        /// </summary>
-        K1,
-
-        /// <summary>
-        /// Knights of the Old Republic II: The Sith Lords (KOTOR 2 / TSL)
-        /// </summary>
-        K2,
-
-        /// <summary>
-        /// Alias for K2 - The Sith Lords
-        /// </summary>
+        K1 = 1,
+        K2 = 2,
+        K1_XBOX = 3,
+        K2_XBOX = 4,
+        K1_IOS = 5,
+        K2_IOS = 6,
+        K1_ANDROID = 7,
+        K2_ANDROID = 8,
         TSL = K2
     }
 
     public static class GameExtensions
     {
-        public static bool IsK1(this Game game) => game == Game.K1;
-        public static bool IsK2(this Game game) => game == Game.K2;
-        public static bool IsTSL(this Game game) => game == Game.K2;
+        public static bool IsK1(this Game game)
+        {
+            return ((int)game) % 2 != 0;
+        }
 
-        /// <summary>
-        /// Checks if this is an iOS version of the game.
-        /// Note: Currently not supported, but included for future compatibility.
-        /// </summary>
-        public static bool IsIOS(this Game game) => false; // Not currently supported
+        public static bool IsK2(this Game game)
+        {
+            return ((int)game) % 2 == 0;
+        }
+
+        public static bool IsTSL(this Game game)
+        {
+            return game == Game.K2;
+        }
+
+        public static bool IsXbox(this Game game)
+        {
+            return game == Game.K1_XBOX || game == Game.K2_XBOX;
+        }
+
+        public static bool IsPc(this Game game)
+        {
+            return game == Game.K1 || game == Game.K2;
+        }
+
+        public static bool IsMobile(this Game game)
+        {
+            return game == Game.K1_IOS || game == Game.K2_IOS || game == Game.K1_ANDROID || game == Game.K2_ANDROID;
+        }
+
+        public static bool IsAndroid(this Game game)
+        {
+            return game == Game.K1_ANDROID || game == Game.K2_ANDROID;
+        }
+
+        public static bool IsIOS(this Game game)
+        {
+            return game == Game.K1_IOS || game == Game.K2_IOS;
+        }
     }
 }
-
