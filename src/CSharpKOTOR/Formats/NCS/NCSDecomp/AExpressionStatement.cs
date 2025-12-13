5f9b3ca955ee38b749722d3394354583172b69d3
Benjamin Auquite <halomastar@gmail.com>
2025-12-12 19:10:37 -0600

    fix: add missing source reference comments to AReturnStatement and AExpressionStatement for 1:1 parity with DeNCS

--- src/CSharpKOTOR/Formats/NCS/NCSDecomp/AExpressionStatement.cs
+++ src/CSharpKOTOR/Formats/NCS/NCSDecomp/AExpressionStatement.cs
@@ -1,21 +1,23 @@
-// 
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-
+// Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/scriptnode/AExpressionStatement.java:8-40
+// Original: public class AExpressionStatement extends ScriptNode
 namespace CSharpKOTOR.Formats.NCS.NCSDecomp.Scriptnode
 {
     public class AExpressionStatement : ScriptNode
     {
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/scriptnode/AExpressionStatement.java:9
+        // Original: private AExpression exp;
         private AExpression exp;
+
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/scriptnode/AExpressionStatement.java:11-14
+        // Original: public AExpressionStatement(AExpression exp) { exp.parent(this); this.exp = exp; }
         public AExpressionStatement(AExpression exp)
         {
             exp.Parent(this);
             this.exp = exp;
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/scriptnode/AExpressionStatement.java:16-18
+        // Original: public AExpression exp() { return this.exp; }
         public virtual AExpression Exp()
         {
             return this.exp;
@@ -28,6 +30,8 @@ namespace CSharpKOTOR.Formats.NCS.NCSDecomp.Scriptnode
             return this.tabs + this.exp.ToString() + ";" + this.newline;
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/scriptnode/AExpressionStatement.java:25-29
+        // Original: @Override public void parent(ScriptNode parent) { super.parent(parent); this.exp.parent(this); }
         public override void Parent(ScriptNode parent)
         {
             base.Parent(parent);
--- src/CSharpKOTOR/Formats/NCS/NCSDecomp/AReturnStatement.cs
+++ src/CSharpKOTOR/Formats/NCS/NCSDecomp/AReturnStatement.cs
@@ -1,31 +1,38 @@
-// 
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
+// Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/scriptnode/AReturnStatement.java:8-43
+// Original: public class AReturnStatement extends ScriptNode
 using CSharpKOTOR.Formats.NCS.NCSDecomp.ScriptNode;
 
 namespace CSharpKOTOR.Formats.NCS.NCSDecomp.Scriptnode
 {
     public class AReturnStatement : ScriptNode
     {
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/scriptnode/AReturnStatement.java:9
+        // Original: protected AExpression returnexp;
         protected AExpression returnexp;
+
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/scriptnode/AReturnStatement.java:11-12
+        // Original: public AReturnStatement() { }
         public AReturnStatement()
         {
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/scriptnode/AReturnStatement.java:14-16
+        // Original: public AReturnStatement(AExpression returnexp) { this.returnexp(returnexp); }
         public AReturnStatement(AExpression returnexp)
         {
             this.Returnexp(returnexp);
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/scriptnode/AReturnStatement.java:18-21
+        // Original: public void returnexp(AExpression returnexp) { returnexp.parent(this); this.returnexp = returnexp; }
         public virtual void Returnexp(AExpression returnexp)
         {
             returnexp.Parent(this);
             this.returnexp = returnexp;
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/scriptnode/AReturnStatement.java:23-25
+        // Original: public AExpression exp() { return this.returnexp; }
         public virtual AExpression Exp()
         {
             return this.returnexp;
@@ -40,6 +47,8 @@ namespace CSharpKOTOR.Formats.NCS.NCSDecomp.Scriptnode
                 : this.tabs + "return " + ExpressionFormatter.FormatValue(this.returnexp) + ";" + this.newline;
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/scriptnode/AReturnStatement.java:34-42
+        // Original: @Override public void close() { super.close(); if (this.returnexp != null) { ((ScriptNode)this.returnexp).close(); } this.returnexp = null; }
         public override void Close()
         {
             base.Close();
