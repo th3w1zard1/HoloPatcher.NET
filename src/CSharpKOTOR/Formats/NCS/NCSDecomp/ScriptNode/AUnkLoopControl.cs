7e3732b755a3f5b25c49dc2ccd5329161f4015dd
Benjamin Auquite <halomastar@gmail.com>
2025-12-12 19:26:42 -0600

    fix: correct AVarRef and AUnkLoopControl to match Java implementation exactly - remove extra methods, fix field visibility, fix toString format, and add source reference comments for 1:1 parity with DeNCS

--- src/CSharpKOTOR/Formats/NCS/NCSDecomp/ScriptNode/AUnkLoopControl.cs
+++ src/CSharpKOTOR/Formats/NCS/NCSDecomp/ScriptNode/AUnkLoopControl.cs
@@ -1,27 +1,32 @@
+// Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/scriptnode/AUnkLoopControl.java:8-23
+// Original: public class AUnkLoopControl extends ScriptNode
 namespace CSharpKOTOR.Formats.NCS.NCSDecomp.ScriptNode
 {
     public class AUnkLoopControl : ScriptNode
     {
-        private int _dest;
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/scriptnode/AUnkLoopControl.java:9
+        // Original: protected int dest;
+        protected int dest;
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/scriptnode/AUnkLoopControl.java:11-13
+        // Original: public AUnkLoopControl(int dest) { this.dest = dest; }
         public AUnkLoopControl(int dest)
         {
-            _dest = dest;
+            this.dest = dest;
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/scriptnode/AUnkLoopControl.java:15-17
+        // Original: public int getDestination() { return this.dest; }
         public int GetDestination()
         {
-            return _dest;
+            return this.dest;
         }
 
-        public void SetDestination(int dest)
-        {
-            _dest = dest;
-        }
-
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/scriptnode/AUnkLoopControl.java:19-22
+        // Original: @Override public String toString() { return "BREAK or CONTINUE undetermined"; }
         public override string ToString()
         {
-            return this.tabs + "BREAK or CONTINUE undetermined" + this.newline;
+            return "BREAK or CONTINUE undetermined";
         }
     }
 }
--- src/CSharpKOTOR/Formats/NCS/NCSDecomp/ScriptNode/AVarRef.cs
+++ src/CSharpKOTOR/Formats/NCS/NCSDecomp/ScriptNode/AVarRef.cs
@@ -1,4 +1,5 @@
-using System;
+// Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/scriptnode/AVarRef.java:13-68
+// Original: public class AVarRef extends ScriptNode implements AExpression
 using CSharpKOTOR.Formats.NCS.NCSDecomp.Scriptnode;
 using CSharpKOTOR.Formats.NCS.NCSDecomp.Stack;
 using CSharpKOTOR.Formats.NCS.NCSDecomp.Utils;
@@ -8,83 +9,91 @@ namespace CSharpKOTOR.Formats.NCS.NCSDecomp.ScriptNode
 {
     public class AVarRef : ScriptNode, AExpression
     {
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/scriptnode/AVarRef.java:14
+        // Original: private Variable var;
         private Variable var;
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/scriptnode/AVarRef.java:16-18
+        // Original: public AVarRef(Variable var) { this.var(var); }
         public AVarRef(Variable var)
         {
-            this.var = var;
+            this.Var(var);
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/scriptnode/AVarRef.java:20-22
+        // Original: public AVarRef(VarStruct struct) { this.var(struct); }
         public AVarRef(VarStruct structVar)
         {
-            this.var = structVar;
+            this.Var(structVar);
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/scriptnode/AVarRef.java:24-26
+        // Original: public Type type() { return this.var.type(); }
         public UtilsType Type()
         {
             return this.var.Type();
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/scriptnode/AVarRef.java:28-30
+        // Original: public Variable var() { return this.var; }
         public Variable Var()
         {
             return this.var;
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/scriptnode/AVarRef.java:32-34
+        // Original: public void var(Variable var) { this.var = var; }
         public void Var(Variable var)
         {
             this.var = var;
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/scriptnode/AVarRef.java:36-42
+        // Original: public void chooseStructElement(Variable var) { if (VarStruct.class.isInstance(this.var) && ((VarStruct)this.var).contains(var)) { this.var = var; } else { throw new RuntimeException("Attempted to select a struct element not in struct"); } }
         public void ChooseStructElement(Variable var)
         {
             if (this.var is VarStruct varStruct && varStruct.Contains(var))
             {
                 this.var = var;
-                return;
             }
-            throw new System.Exception("Attempted to select a struct element not in struct");
+            else
+            {
+                throw new System.Exception("Attempted to select a struct element not in struct");
+            }
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/scriptnode/AVarRef.java:44-47
+        // Original: @Override public String toString() { return this.var.toString(); }
         public override string ToString()
         {
             return this.var.ToString();
         }
 
-        public StackEntry StackEntry()
-        {
-            return this.var;
-        }
-
-        public void StackEntry(StackEntry stackentry)
-        {
-            this.var = (Variable)stackentry;
-        }
-
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/scriptnode/AVarRef.java:49-52
+        // Original: @Override public StackEntry stackentry() { return this.var; }
         public StackEntry Stackentry()
         {
             return this.var;
         }
 
-        public void Stackentry(StackEntry p0)
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/scriptnode/AVarRef.java:54-57
+        // Original: @Override public void stackentry(StackEntry stackentry) { this.var((Variable)stackentry); }
+        public void Stackentry(StackEntry stackentry)
         {
-            this.var = (Variable)p0;
+            this.Var((Variable)stackentry);
         }
 
         Scriptnode.ScriptNode AExpression.Parent() => (Scriptnode.ScriptNode)(object)base.Parent();
         void AExpression.Parent(Scriptnode.ScriptNode p0) => base.Parent((ScriptNode)(object)p0);
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/scriptnode/AVarRef.java:59-67
+        // Original: @Override public void close() { super.close(); if (this.var != null) { this.var.close(); } this.var = null; }
         public override void Close()
         {
             base.Close();
             if (this.var != null)
             {
-                // Variable may have Close() method, but we check for it dynamically
-                var varType = this.var.GetType();
-                var closeMethod = varType.GetMethod("Close", System.Type.EmptyTypes);
-                if (closeMethod != null)
-                {
-                    closeMethod.Invoke(this.var, null);
-                }
+                this.var.Close();
             }
             this.var = null;
         }
