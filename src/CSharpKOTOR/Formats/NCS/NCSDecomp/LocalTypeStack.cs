e30236daed4aacf81d5525ad19c414acba169055
Benjamin Auquite <halomastar@gmail.com>
2025-12-12 21:08:05 -0600

    fix: add missing source comments and fix logic differences in LocalTypeStack.cs to match DeNCS Java source exactly - use Math.Min in remove, check isEmpty in removeParams, use JavaSystem.getProperty for line separator

--- src/CSharpKOTOR/Formats/NCS/NCSDecomp/LocalTypeStack.cs
+++ src/CSharpKOTOR/Formats/NCS/NCSDecomp/LocalTypeStack.cs
@@ -6,6 +6,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using CSharpKOTOR.Formats.NCS.NCSDecomp.Utils;
+using CSharpKOTOR.Formats.NCS.NCSDecomp.AST;
 using UtilsType = CSharpKOTOR.Formats.NCS.NCSDecomp.Utils.Type;
 namespace CSharpKOTOR.Formats.NCS.NCSDecomp.Stack
 {
@@ -18,10 +19,13 @@ namespace CSharpKOTOR.Formats.NCS.NCSDecomp.Stack
             this.stack.AddFirst(type);
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/stack/LocalTypeStack.java:21-38
+        // Original: public Type get(int offset) { ... return new Type((byte)-1); }
         public virtual UtilsType Get(int offset)
         {
             ListIterator it = this.stack.ListIterator();
             int pos = 0;
+
             while (it.HasNext())
             {
                 UtilsType type = (UtilsType)it.Next();
@@ -40,10 +44,13 @@ namespace CSharpKOTOR.Formats.NCS.NCSDecomp.Stack
             return new UtilsType(unchecked((byte)(-1)));
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/stack/LocalTypeStack.java:40-64
+        // Original: public Type get(int offset, SubroutineState state) { ... }
         public virtual UtilsType Get(int offset, SubroutineState state)
         {
             ListIterator it = this.stack.ListIterator();
             int pos = 0;
+
             while (it.HasNext())
             {
                 UtilsType type = (UtilsType)it.Next();
@@ -61,46 +68,55 @@ namespace CSharpKOTOR.Formats.NCS.NCSDecomp.Stack
 
             if (state.IsPrototyped())
             {
-                UtilsType type = state.GetParamType(offset - pos);
-                if (!type.Equals((byte)0))
+                UtilsType typex = state.GetParamType(offset - pos);
+                if (!typex.Equals((byte)0))
                 {
-                    return type;
+                    return typex;
                 }
             }
 
             return new UtilsType(unchecked((byte)(-1)));
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/stack/LocalTypeStack.java:66-71
+        // Original: public void remove(int count) { int actualCount = Math.min(count, this.stack.size()); for (int i = 0; i < actualCount; i++) { this.stack.removeFirst(); } }
         public virtual void Remove(int count)
         {
-            for (int i = 0; i < count; ++i)
+            int actualCount = Math.Min(count, this.stack.Count);
+            for (int i = 0; i < actualCount; i++)
             {
                 this.stack.RemoveFirst();
             }
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/stack/LocalTypeStack.java:73-82
+        // Original: public void removeParams(int count, SubroutineState state) { LinkedList<Type> params = new LinkedList<>(); for (int i = 0; i < count; i++) { Type type = this.stack.isEmpty() ? new Type((byte)-1) : this.stack.removeFirst(); params.addFirst(type); } state.updateParams(params); }
         public virtual void RemoveParams(int count, SubroutineState state)
         {
             LinkedList @params = new LinkedList();
-            for (int i = 0; i < count; ++i)
+
+            for (int i = 0; i < count; i++)
             {
-                UtilsType type = (UtilsType)this.stack.RemoveFirst();
+                UtilsType type = this.stack.Count == 0 ? new UtilsType(unchecked((byte)(-1))) : (UtilsType)this.stack.RemoveFirst();
                 @params.AddFirst(type);
             }
 
             state.UpdateParams(@params);
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/stack/LocalTypeStack.java:84-99
+        // Original: public int removePrototyping(int count) { ... }
         public virtual int RemovePrototyping(int count)
         {
             int @params = 0;
             int i = 0;
+
             while (i < count)
             {
                 if (this.stack.Count == 0)
                 {
-                    ++@params;
-                    ++i;
+                    @params++;
+                    i++;
                 }
                 else
                 {
@@ -112,34 +128,41 @@ namespace CSharpKOTOR.Formats.NCS.NCSDecomp.Stack
             return @params;
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/stack/LocalTypeStack.java:101-107
+        // Original: public void remove(int start, int count) { int loc = start - 1; for (int i = 0; i < count; i++) { this.stack.remove(loc); } }
         public virtual void Remove(int start, int count)
         {
             int loc = start - 1;
-            for (int i = 0; i < count; ++i)
+
+            for (int i = 0; i < count; i++)
             {
                 this.stack.Remove(loc);
             }
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/stack/LocalTypeStack.java:109-122
+        // Original: @Override public String toString() { ... }
         public override string ToString()
         {
-            string newline = Environment.NewLine;
+            string newline = JavaSystem.GetProperty("line.separator");
             StringBuilder buffer = new StringBuilder();
             int max = this.stack.Count;
-            buffer.Append("---stack, size " + max + "---" + newline);
-            for (int i = 1; i <= max; ++i)
+            buffer.Append("---stack, size " + max.ToString() + "---" + newline);
+
+            for (int i = 1; i <= max; i++)
             {
                 UtilsType type = (UtilsType)this.stack[max - i];
-                buffer.Append("-->" + i + " is type " + type + newline);
+                buffer.Append("-->" + i.ToString() + " is type " + type + newline);
             }
 
             return buffer.ToString();
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/stack/LocalTypeStack.java:124-129
+        // Original: @Override public LocalTypeStack clone() { LocalTypeStack newStack = new LocalTypeStack(); newStack.stack = new LinkedList<>(this.stack); return newStack; }
         public override object Clone()
         {
             LocalTypeStack newStack = new LocalTypeStack();
-            // Clone the custom LinkedList
             newStack.stack = new LinkedList();
             var it = this.stack.Iterator();
             while (it.HasNext())
