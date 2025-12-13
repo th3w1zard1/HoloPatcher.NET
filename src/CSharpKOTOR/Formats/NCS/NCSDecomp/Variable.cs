0d27985249550bbe13ba9358eaae7e1508279e71
Benjamin Auquite <halomastar@gmail.com>
2025-12-12 21:01:30 -0600

    fix: add missing source comments and remove extra comments in Variable.cs, fix GetElement and CompareTo logic to match DeNCS Java source exactly

--- src/CSharpKOTOR/Formats/NCS/NCSDecomp/Variable.cs
+++ src/CSharpKOTOR/Formats/NCS/NCSDecomp/Variable.cs
@@ -48,16 +48,22 @@ namespace CSharpKOTOR.Formats.NCS.NCSDecomp.Stack
             this.varstruct = null;
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/stack/Variable.java:45-47
+        // Original: @Override public void doneParse() { this.stackcounts = null; }
         public override void DoneParse()
         {
             this.stackcounts = null;
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/stack/Variable.java:50-52
+        // Original: @Override public void doneWithStack(LocalVarStack stack) { this.stackcounts.remove(stack); }
         public override void DoneWithStack(LocalVarStack stack)
         {
             this.stackcounts.Remove(stack);
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/stack/Variable.java:54-60
+        // Original: public void isReturn(boolean isreturn) { if (isreturn) { this.function = 1; } else { this.function = 0; } }
         public virtual void IsReturn(bool isreturn)
         {
             if (isreturn)
@@ -70,6 +76,8 @@ namespace CSharpKOTOR.Formats.NCS.NCSDecomp.Stack
             }
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/stack/Variable.java:62-68
+        // Original: public void isParam(boolean isparam) { if (isparam) { this.function = 2; } else { this.function = 0; } }
         public virtual void IsParam(bool isparam)
         {
             if (isparam)
@@ -82,41 +90,57 @@ namespace CSharpKOTOR.Formats.NCS.NCSDecomp.Stack
             }
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/stack/Variable.java:70-72
+        // Original: public boolean isReturn() { return this.function == 1; }
         public virtual bool IsReturn()
         {
             return this.function == 1;
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/stack/Variable.java:74-76
+        // Original: public boolean isParam() { return this.function == 2; }
         public virtual bool IsParam()
         {
             return this.function == 2;
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/stack/Variable.java:78-80
+        // Original: public void assigned() { this.assigned = true; }
         public virtual void Assigned()
         {
             this.assigned = true;
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/stack/Variable.java:82-84
+        // Original: public boolean isAssigned() { return this.assigned; }
         public virtual bool IsAssigned()
         {
             return this.assigned;
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/stack/Variable.java:86-88
+        // Original: public boolean isStruct() { return this.varstruct != null; }
         public virtual bool IsStruct()
         {
             return this.varstruct != null;
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/stack/Variable.java:90-92
+        // Original: public void varstruct(VarStruct varstruct) { this.varstruct = varstruct; }
         public virtual void Varstruct(VarStruct varstruct)
         {
             this.varstruct = varstruct;
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/stack/Variable.java:94-96
+        // Original: public VarStruct varstruct() { return this.varstruct; }
         public virtual VarStruct Varstruct()
         {
             return this.varstruct;
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/stack/Variable.java:98-106
+        // Original: @Override public void addedToStack(LocalStack<?> stack) { ... }
         public override void AddedToStack(LocalStack stack)
         {
             object countObj;
@@ -132,6 +156,8 @@ namespace CSharpKOTOR.Formats.NCS.NCSDecomp.Stack
             }
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/stack/Variable.java:108-116
+        // Original: @Override public void removedFromStack(LocalStack<?> stack) { ... }
         public override void RemovedFromStack(LocalStack stack)
         {
             object countObj;
@@ -180,49 +206,50 @@ namespace CSharpKOTOR.Formats.NCS.NCSDecomp.Stack
             return count > 0;
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/stack/Variable.java:128-130
+        // Original: public void name(String prefix, byte hint) { this.name = prefix + this.type.toString() + Byte.toString(hint); }
         public virtual void Name(string prefix, byte hint)
         {
-            this.name = prefix.ToString() + this.type.ToString() + hint.ToString();
+            this.name = prefix + this.type.ToString() + hint.ToString();
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/stack/Variable.java:132-134
+        // Original: public void name(String infix, int hint) { this.name = this.type.toString() + infix + Integer.toString(hint); }
         public virtual void Name(string infix, int hint)
         {
-            this.name = this.type.ToString() + infix + hint;
+            this.name = this.type.ToString() + infix + hint.ToString();
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/stack/Variable.java:136-138
+        // Original: public void name(String name) { this.name = name; }
         public virtual void Name(string name)
         {
             this.name = name;
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/stack/Variable.java:141-147
+        // Original: @Override public StackEntry getElement(int stackpos) { if (stackpos != 1) { throw new RuntimeException("Position > 1 for var, not struct"); } else { return this; } }
         public override StackEntry GetElement(int stackpos)
         {
-
-            // For simple variables (non-struct), any position >= 1 returns this variable
-            // The position calculation in LocalVarStack/VarStruct can produce values > 1
-            // even for size-1 variables due to the formula (pos - offset + 1)
-            // We treat simple variables as "atomic" - any sub-element access returns the whole variable
-            if (stackpos < 1)
+            if (stackpos != 1)
             {
-                throw new Exception("Position " + stackpos + " must be >= 1");
+                throw new Exception("Position > 1 for var, not struct");
             }
-
-            return this;
+            else
+            {
+                return this;
+            }
         }
 
-        // For simple variables (non-struct), any position >= 1 returns this variable
-        // The position calculation in LocalVarStack/VarStruct can produce values > 1
-        // even for size-1 variables due to the formula (pos - offset + 1)
-        // We treat simple variables as "atomic" - any sub-element access returns the whole variable
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/stack/Variable.java:149-151
+        // Original: public String toDebugString() { return "type: " + this.type + " name: " + this.name + " assigned: " + Boolean.toString(this.assigned); }
         public virtual string ToDebugString()
         {
             return "type: " + this.type + " name: " + this.name + " assigned: " + this.assigned.ToString();
         }
 
-        // For simple variables (non-struct), any position >= 1 returns this variable
-        // The position calculation in LocalVarStack/VarStruct can produce values > 1
-        // even for size-1 variables due to the formula (pos - offset + 1)
-        // We treat simple variables as "atomic" - any sub-element access returns the whole variable
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/stack/Variable.java:154-161
+        // Original: @Override public String toString() { if (this.varstruct != null) { this.varstruct.updateNames(); return this.varstruct.name() + "." + this.name; } else { return this.name; } }
         public override string ToString()
         {
             if (this.varstruct != null)
@@ -234,53 +261,42 @@ namespace CSharpKOTOR.Formats.NCS.NCSDecomp.Stack
             return this.name;
         }
 
-        // For simple variables (non-struct), any position >= 1 returns this variable
-        // The position calculation in LocalVarStack/VarStruct can produce values > 1
-        // even for size-1 variables due to the formula (pos - offset + 1)
-        // We treat simple variables as "atomic" - any sub-element access returns the whole variable
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/stack/Variable.java:163-165
+        // Original: public String toDeclString() { return this.type + " " + this.name; }
         public virtual string ToDeclString()
         {
             return this.type + " " + this.name;
         }
 
-        // For simple variables (non-struct), any position >= 1 returns this variable
-        // The position calculation in LocalVarStack/VarStruct can produce values > 1
-        // even for size-1 variables due to the formula (pos - offset + 1)
-        // We treat simple variables as "atomic" - any sub-element access returns the whole variable
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/stack/Variable.java:168-178
+        // Original: @Override public int compareTo(Variable o) throws ClassCastException { if (o == null) { throw new NullPointerException(); } else if (this == o) { return 0; } else if (this.name == null) { return -1; } else { return o.name == null ? 1 : this.name.compareTo(o.name); } }
         public virtual int CompareTo(object o)
         {
             if (o == null)
             {
                 throw new NullReferenceException();
             }
-
-            if (!typeof(Variable).IsInstanceOfType(o))
+            else if (!typeof(Variable).IsInstanceOfType(o))
             {
                 throw new InvalidCastException();
             }
-
-            if (this == o)
+            else if (this == o)
             {
                 return 0;
             }
-
-            if (this.name == null)
+            else if (this.name == null)
             {
                 return -1;
             }
-
-            if (((Variable)o).name == null)
+            else
             {
-                return 1;
+                Variable other = (Variable)o;
+                return other.name == null ? 1 : this.name.CompareTo(other.name);
             }
-
-            return this.name.CompareTo(((Variable)o).name);
         }
 
-        // For simple variables (non-struct), any position >= 1 returns this variable
-        // The position calculation in LocalVarStack/VarStruct can produce values > 1
-        // even for size-1 variables due to the formula (pos - offset + 1)
-        // We treat simple variables as "atomic" - any sub-element access returns the whole variable
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/stack/Variable.java:180-185
+        // Original: public void stackWasCloned(LocalStack<?> oldstack, LocalStack<?> newstack) { Integer count = this.stackcounts.get(oldstack); if (count != null && count > 0) { this.stackcounts.put(newstack, count); } }
         public virtual void StackWasCloned(LocalStack oldstack, LocalStack newstack)
         {
             object countObj;
