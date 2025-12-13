6a2d4bf7e9fa3eedde62b8915cc1e0a8df1d229e
Benjamin Auquite <halomastar@gmail.com>
2025-12-12 19:03:54 -0600

    fix: add missing source reference comments to SetPositions for 1:1 parity with DeNCS

--- src/CSharpKOTOR/Formats/NCS/NCSDecomp/SetPositions.cs
+++ src/CSharpKOTOR/Formats/NCS/NCSDecomp/SetPositions.cs
@@ -1,9 +1,5 @@
-// 
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
+// Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/utils/SetPositions.java:14-39
+// Original: public class SetPositions extends PrunedReversedDepthFirstAdapter
 using CSharpKOTOR.Formats.NCS.NCSDecomp;
 using CSharpKOTOR.Formats.NCS.NCSDecomp.Analysis;
 
@@ -11,19 +7,30 @@ namespace CSharpKOTOR.Formats.NCS.NCSDecomp.Utils
 {
     public class SetPositions : PrunedReversedDepthFirstAdapter
     {
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/utils/SetPositions.java:15
+        // Original: private NodeAnalysisData nodedata;
         private NodeAnalysisData nodedata;
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/utils/SetPositions.java:16
+        // Original: private int currentPos;
         private int currentPos;
+
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/utils/SetPositions.java:18-21
+        // Original: public SetPositions(NodeAnalysisData nodedata) { this.nodedata = nodedata; this.currentPos = 0; }
         public SetPositions(NodeAnalysisData nodedata)
         {
             this.nodedata = nodedata;
             this.currentPos = 0;
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/utils/SetPositions.java:23-25
+        // Original: public void done() { this.nodedata = null; }
         public virtual void Done()
         {
             this.nodedata = null;
         }
 
+        // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/utils/SetPositions.java:27-33
+        // Original: @Override public void defaultIn(Node node) { int pos = NodeUtils.getCommandPos(node); if (pos > 0) { this.currentPos = pos; } }
         public override void DefaultIn(Node node)
         {
             int pos = NodeUtils.GetCommandPos(node);
