5e066a80a060181c0f8af520454fb91a044aba2b
Benjamin Auquite <halomastar@gmail.com>
2025-12-12 21:49:42 -0600

    revert: set root end back to 0 to match vendor code exactly

--- src/CSharpKOTOR/Formats/NCS/NCSDecomp/SubScriptState.cs
+++ src/CSharpKOTOR/Formats/NCS/NCSDecomp/SubScriptState.cs
@@ -67,9 +67,9 @@ namespace CSharpKOTOR.Formats.NCS.NCSDecomp.Scriptutils
             this.subdata = subdata;
             this.state = 0;
             this.vardecs = new HashMap();
-            // Matching DeNCS implementation: for globals, use ASub(0, 0) which sets start=0, end=0
-            // Use a large end value to prevent CheckEnd from moving away from root when processing nodes at position 0
-            this.root = new ScriptNode.ASub(0, null, null, 0, int.MaxValue);
+            // Matching DeNCS implementation at vendor/DeNCS/src/main/java/com/kotor/resource/formats/ncs/scriptutils/SubScriptState.java:129
+            // Original: this.root = new ASub(0, 0);
+            this.root = new ScriptNode.ASub(0, null, null, 0, 0);
             this.current = this.root;
             this.stack = stack;
             this.varcounts = new HashMap();
